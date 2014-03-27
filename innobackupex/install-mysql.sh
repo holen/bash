cf17d80092426c10cb5ff809d4837b58431f4b6d
holen <yh_zhl@sina.com>
2014-03-27 14:33:28 +0800

    mysql

--- innobackupex/install-mysql.sh
+++ innobackupex/install-mysql.sh
@@ -12,15 +12,16 @@ chown -R mysql.root /data/mysql
 chown -R mysql.root /data/mysql-tmp
 chown -R mysql.root /data/mysql-log
 
-sed -i "/device/a \ \ \/data\/mysql-log\/* rw," user.sbin.mysqld
-sed -i "/device/a \ \ \/data\/mysqllog\/ r," user.sbin.mysqld
-sed -i "/device/a \ \ \/data\/mysql-tmp\/* rw," user.sbin.mysqld
-sed -i "/device/a \ \ \/data\/mysql-tmp\/ r," user.sbin.mysqld
-sed -i "/device/a \ \ \/data\/mysql\/** rwk," user.sbin.mysqld
-sed -i "/device/a \ \ \/data\/mysql\/ r," user.sbin.mysqld
+sed -i "/device/a \ \ \/data\/mysql-log\/* rw," /etc/apparmor.d/usr.sbin.mysqld
+sed -i "/device/a \ \ \/data\/mysqllog\/ r," /etc/apparmor.d/usr.sbin.mysqld
+sed -i "/device/a \ \ \/data\/mysql-tmp\/* rw," /etc/apparmor.d/usr.sbin.mysqld
+sed -i "/device/a \ \ \/data\/mysql-tmp\/ r," /etc/apparmor.d/usr.sbin.mysqld
+sed -i "/device/a \ \ \/data\/mysql\/** rwk," /etc/apparmor.d/usr.sbin.mysqld
+sed -i "/device/a \ \ \/data\/mysql\/ r," /etc/apparmor.d/usr.sbin.mysqld
 
-sed -i 's/^datadir.*/datadir=\/data\/mysql/g' /etc/mysql/my.cnf
-sed -i 's/^tmpdir.*/tmpdir=\/data\/mysql-tmp/g' /etc/mysql/my.cnf
-sed -i 's/^log_error.*/log_error=\/data\/mysql-log\/error\.log/g' /etc/mysql/my.cnf
+sed -i 's/^datadir.*/datadir    =   \/data\/mysql/g' /etc/mysql/my.cnf
+sed -i 's/^tmpdir.*/tmpdir  =   \/data\/mysql-tmp/g' /etc/mysql/my.cnf
+sed -i 's/^log_error.*/log_error    =   \/data\/mysql-log\/error\.log/g' /etc/mysql/my.cnf
+sed -i 's/^bind-address.*/bind-address            = 0.0.0.0/g' /etc/mysql/my.cnf
 #slow_query_log  = 1
 #slow_query_log_file     = /data/mysql-log/slow-query.log
