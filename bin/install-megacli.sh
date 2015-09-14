c7e79bdeed5d250bed07854afa38b2e7b3d90db4
holen <yh_zhl@sina.com>
2015-09-14 14:28:25 +0800

    bash

--- bin/install-megacli.sh
+++ bin/install-megacli.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-echo "deb http://hwraid.le-vert.net/ubuntu/ $(lsb_release -s -c) main" >> /etc/apt/sources.list
+if [ `uname -v | grep Ubuntu | wc -l` == "1" ];
+then
+    echo "deb http://hwraid.le-vert.net/ubuntu/ $(lsb_release -s -c) main" >> /etc/apt/sources.list
+else
+    echo "deb http://hwraid.le-vert.net/debian/ $(lsb_release -s -c) main" >> /etc/apt/sources.list
+fi
 wget -O - http://hwraid.le-vert.net/debian/hwraid.le-vert.net.gpg.key | apt-key add -
 apt-get update 
 apt-get install megacli -y
